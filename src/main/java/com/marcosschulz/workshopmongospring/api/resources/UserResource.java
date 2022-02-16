dc57a855983f32b4474aa3bc6efea11b13651136
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-16 12:06:39 -0300

    Adicionado UserDTO para retornar usuarios

--- src/main/java/com/marcosschulz/workshopmongospring/api/resources/UserResource.java
+++ src/main/java/com/marcosschulz/workshopmongospring/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.marcosschulz.workshopmongospring.api.resources;
 
+import com.marcosschulz.workshopmongospring.domain.dto.UserDTO;
 import com.marcosschulz.workshopmongospring.domain.models.User;
 import com.marcosschulz.workshopmongospring.domain.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,8 +23,9 @@ public class UserResource {
     UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> userList = service.findAll();
-        return ResponseEntity.ok().body(userList);
+        List<UserDTO> userDTOList = userList.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOList);
     }
 }
