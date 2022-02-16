bdd7a9c28f0d9d7144d03211c420f6012429b81e
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-16 10:25:15 -0300

    Projeto criado

---
+++ src/test/java/com/marcosschulz/workshopmongospring/WorkshopMongoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosschulz.workshopmongospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
